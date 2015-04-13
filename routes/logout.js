888e9b8ec83911a443f4b820221232b5bcd8c42c
p1ch-jp <p1ch.jp@gmail.com>
2015-04-13 22:24:18 +0900

    Change logout
    ログアウト時に自動でトップにリダイレクトするように変更した

--- routes/logout.js
+++ routes/logout.js
@@ -4,9 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/logout', function(req, res, next) {
   req.session.destroy(function(err) {
-    res.render('logout', {
-      title: 'POOKIE'
-    });
+    res.redirect('/');
   });
 });
 
